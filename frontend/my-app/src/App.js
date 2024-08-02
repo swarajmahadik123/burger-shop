ce99a5a8331e5625e0a541c3ff0d0bc73b170772
swarajmahadik123 <swarajmahadik1234@gmail.com>
2024-08-03 00:04:41 +0530

    added tailwind

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,20 @@
+
+// import './App.css';
+// import Home from './Components/Home';
+
+// function App() {
+//   return (
+//     <>
+//       <Home/>
+//     </>
+//   );
+// }
+
+// export default App;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
