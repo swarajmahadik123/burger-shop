d03164615ce75c689405b5e1dd7bafb52fd1907b
swarajmahadik123 <swarajmahadik1234@gmail.com>
2024-08-17 13:42:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://burger-shop-mauve.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
